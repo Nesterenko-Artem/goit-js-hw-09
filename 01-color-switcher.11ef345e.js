d03293a24be5d49e44c245afd090f554eee0d495
Nesterenko-Artem <100300307+Nesterenko-Artem@users.noreply.github.com>
2022-08-13 12:38:14 +0000

    Deploying to gh-pages from @ Nesterenko-Artem/goit-js-hw-09@75d0aaaac49be6d503b033f2883c8f399dda4ec4 🚀

---
+++ 01-color-switcher.11ef345e.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body");t.disabled=!1,e.disabled=!0;var d=null;function a(){n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}t.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1,d=setInterval((function(){a()}),1e3)})),e.addEventListener("click",(function(){t.disabled=!1,e.disabled=!0,clearInterval(d)}))}();
+//# sourceMappingURL=01-color-switcher.11ef345e.js.map
