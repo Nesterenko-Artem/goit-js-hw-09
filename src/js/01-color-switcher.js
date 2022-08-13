75d0aaaac49be6d503b033f2883c8f399dda4ec4
Nesterenko-Artem <petr79petrov1979@gmail.com>
2022-08-13 12:37:19 +0300

    fix task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,35 @@
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+const INTERVAL_CHANGE = 1000;
 
+btnStart.disabled = false;
+btnStop.disabled = true;
+let idInterval = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function changeColor() {
+  body.style.backgroundColor = getRandomHexColor();
+}
+
+function start() {
+  btnStart.disabled = true;
+  btnStop.disabled = false;
+
+  idInterval = setInterval(() => {
+    changeColor();
+  }, INTERVAL_CHANGE);
+}
+
+function stop() {
+  btnStart.disabled = false;
+  btnStop.disabled = true;
+
+  clearInterval(idInterval);
+}
+
+btnStart.addEventListener('click', start);
+btnStop.addEventListener('click', stop);
