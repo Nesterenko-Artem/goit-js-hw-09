0a9fa63ef620027ad66b7c01c90aa9d191ba3a56
Nesterenko-Artem <petr79petrov1979@gmail.com>
2022-08-14 12:45:17 +0300

    fix task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,22 +1,46 @@
 import Notiflix from 'notiflix';
-
-
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-   Notiflix.Notify.success('Sol lucet omnibus');
-  } else {
-    
-Notiflix.Notify.failure('Qui timide rogat docet negare');
-  }
-}
-// Notiflix.Notify.failure('Qui timide rogat docet negare');
-//  Notiflix.Notify.success('Sol lucet omnibus');
+Notiflix.Notify.init({
+  timeout: 10000,
+});
 
 const refs = {
   form: document.querySelector('.form'),
-  delay: document.querySelector('input[name="delay]'),
-  step: document.querySelector('input[name="step]'),
-  amount: document.querySelector('input[name="amount]'),
+  delay: document.querySelector('input[name=delay]'),
+  step: document.querySelector('input[name=step]'),
+  amount: document.querySelector('input[name=amount]'),
 };
 
+refs.form.addEventListener('submit', onFormSabmit);
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    });
+  }, delay);
+}
+
+function onFormSabmit(e) {
+  e.preventDefault();
+  const {
+    elements: { delay, step, amount },
+  } = e.currentTarget;
+
+  let currentDelay = +delay.value;
+  for (let i = 1; i <= amount.value; i++) {
+  createPromise(i, currentDelay)
+    .then(({ position, delay }) =>
+      Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`)
+    )
+    .catch(({ position, delay }) =>
+      Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`)
+    );
+    currentDelay += +step.value 
+  }
+
+}
